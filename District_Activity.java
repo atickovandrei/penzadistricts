5b428811582e78b386a050da89c96a2ee64b2048
atickovandrei <atickov.andrei@yandex.ru>
2019-04-04 00:13:52 +0300

    Add files via upload

---
+++ District_Activity.java
@@ -0,0 +1,45 @@
+package com.example.recyclerview_newversion10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class District_Activity extends AppCompatActivity {
+
+    private TextView tvtitle,tvdescription,tvcategory;
+    private ImageView img, atimg;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_district_);
+
+        tvtitle = (TextView) findViewById(R.id.txttitle);
+        tvdescription = (TextView) findViewById(R.id.txtAbout);
+        //tvcategory = (TextView) findViewById(R.id.txtCat);
+        img = (ImageView) findViewById(R.id.thumbnail);
+        atimg = (ImageView) findViewById(R.id.atimage_id);
+
+
+        Intent intent = getIntent();
+        String Title = intent.getExtras().getString("Title");
+        String Description = intent.getExtras().getString("Description");
+        int Thumbnail = intent.getExtras().getInt("Thumbnail") ;
+        int Imageabout = intent.getExtras().getInt("Imageabout") ;
+
+
+        tvtitle.setText(Title);
+        tvdescription.setText(Description);
+        img.setImageResource(Thumbnail);
+        atimg.setImageResource(Imageabout);
+
+    }
+
+   // List<inform> lstinform;
+
+
+}
